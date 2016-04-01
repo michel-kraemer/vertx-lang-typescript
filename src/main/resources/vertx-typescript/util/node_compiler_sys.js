0492ff3f465a19bc91262d409b5c64b8b74bd42b
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2016-04-01 14:01:16 +0200

    Retry read and write on EAGAIN
    Fixes #1

--- src/main/resources/vertx-typescript/util/node_compiler_sys.js
+++ src/main/resources/vertx-typescript/util/node_compiler_sys.js
@@ -4,6 +4,37 @@
   
   process.stdin.resume();
   
+  function doWriteSync(fd, data) {
+    while (true) {
+      try {
+        fs.writeSync(fd, data);
+        break;
+      } catch (e) {
+        if (e.code === 'EINTR' || e.code === 'EAGAIN') {
+          // resource not available. try again.
+          continue;
+        } else {
+          throw e;
+        }
+      }
+    }
+  }
+  
+  function doReadSync(fd, buffer, offset, length) {
+    while (true) {
+      try {
+        return fs.readSync(fd, buffer, offset, length);
+      } catch (e) {
+        if (e.code === 'EINTR' || e.code === 'EAGAIN') {
+          // resource not available. try again.
+          continue;
+        } else {
+          throw e;
+        }
+      }
+    }
+  }
+  
   // override host to use the lib.core.d.ts instead of lib.d.ts. The latter
   // contains too many definitions that we don't need. In fact WebSocket
   // conflicts with Vert.x WebSocket.
@@ -15,9 +46,9 @@
     };
     host.fileExists = function(path) {
       // send tag and filename to parent process
-      fs.writeSync(process.stdout.fd, "VERTX_TYPESCRIPT_FILEEXISTS" + path + "\n");
+      doWriteSync(process.stdout.fd, "VERTX_TYPESCRIPT_FILEEXISTS" + path + "\n");
       var buf = new Buffer(1);
-      var res = fs.readSync(process.stdin.fd, buf, 0, 1);
+      var res = doReadSync(process.stdin.fd, buf, 0, 1);
       if (res != 1) {
         throw new Error("Could not read boolean from input stream");
       }
@@ -25,17 +56,17 @@
     };
     return host;
   };
-
+  
   ts.sys.readFile = function(fileName, encoding) {
     // send tag and filename to parent process
-    fs.writeSync(process.stdout.fd, "VERTX_TYPESCRIPT_READFILE" + fileName + "\n");
+    doWriteSync(process.stdout.fd, "VERTX_TYPESCRIPT_READFILE" + fileName + "\n");
     
     // read number of bytes to read from stdin
     var res;
     var buf = new Buffer(1);
     var size = "";
     do {
-      res = fs.readSync(process.stdin.fd, buf, 0, 1);
+      res = doReadSync(process.stdin.fd, buf, 0, 1);
       if (res != 1) {
         throw new Error("Could not read size from input stream");
       }
@@ -56,7 +87,7 @@
     buf = new Buffer(size);
     var read = 0;
     while (read < size) {
-      res = fs.readSync(process.stdin.fd, buf, read, size - read);
+      res = doReadSync(process.stdin.fd, buf, read, size - read);
       read += res;
     }
 
@@ -64,7 +95,7 @@
   };
 
   ts.sys.writeFile = function(fileName, data, writeByteOrderMark) {
-    fs.writeSync(process.stdout.fd, data);
+    doWriteSync(process.stdout.fd, data);
   };
 
   ts.sys.getExecutingFilePath = function() {
