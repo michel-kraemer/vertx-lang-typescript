474d0a98f2bc0725d38c9b504c79d91744b7bcb7
daniel <daniel@vizualiiz.com>
2016-07-20 10:11:08 -0700

    Got it updated and  working.

--- src/main/typings/vertx-js/java.d.ts
+++
@@ -1,8 +0,0 @@
-declare var Java: {
-  type(name: string): any;
-  extend(t: any): any;
-  from(arr: any): Array<any>;
-  to(obj: any, t: string): any;
-  super(t: any): any;
-  synchronized<T>(f: T): T;
-}
